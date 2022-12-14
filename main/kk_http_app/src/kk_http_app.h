3e9a9f2bee356e8f4d72af5f5a71296e8695ecb5
k-nowicki <now.nowicki@gmail.com>
2022-12-14 19:26:28 +0100

    HTTP server: File serving from /sd/www dir; data serving for /data/* uris; dir kk_http_app moved from components to main/ as it is part of app and needs app level methods/variables access; IDF-Arduino conflicts fixes;

---
+++ main/kk_http_app/src/kk_http_app.h
@@ -0,0 +1,97 @@
+/*
+ * kk_http.h
+ *
+ *  Created on: 12 12 2022
+ *      Author: Karol Nowicki
+ */
+
+#include <esp_event.h>
+#include <esp_log.h>
+#include <esp_system.h>
+#include <sys/param.h>
+#include <nvs_flash.h>
+#include "esp_netif.h"
+#include "esp_eth.h"
+#include "esp_vfs.h"
+#include "protocol_common.h"
+#include <esp_tls_crypto.h>
+#include <esp_http_server.h>
+#include <esp_https_server.h>
+
+#ifndef COMPONENTS_KK_HTTP_APP_
+#define COMPONENTS_KK_HTTP_APP_
+//#ifdef __cplusplus
+//extern "C" {
+//#endif
+
+
+/// Helper macro for checking file extension
+#define IS_FILE_EXT(filename, ext) (strcasecmp(&filename[strlen(filename) - sizeof(ext) + 1], ext) == 0)
+
+/*#****************************************************************************/
+
+/**
+ * Handler to download a file kept on the server
+ * @param req Request data
+ * @return
+ *      ESP_OK
+ *      ESP_FAIL
+ */
+esp_err_t file_get_handler(httpd_req_t *req);
+
+/**
+ * Handler to redirect incoming GET request for / to /index.html
+ * @param req Request data
+ * @return ESP_OK
+ */
+esp_err_t index_html_get_handler(httpd_req_t *req);
+
+/**
+ * Handler to respond with dynamic data
+ * @param req Request pointer
+ * @return ESP_OK
+ */
+esp_err_t data_get_handler(httpd_req_t *req);
+
+/**
+ * Sends json formatted fresh measurements as a http response
+ *
+ * @param req Request pointer
+ * @return ESP_OK
+ */
+esp_err_t send_current_measurements(httpd_req_t *req);
+
+/**
+ *  Set HTTP response content type according to file extension
+ * @param req Request data
+ * @param filename Pointer to filename
+ * @return Error codes from @see httpd_resp_set_type()
+ *
+ */
+esp_err_t set_content_type_from_file(httpd_req_t *req, const char *filename);
+
+/**
+ * Copies the full path into destination buffer and returns
+ * pointer to path (skipping the preceding base path)
+ */
+/**
+ * Copies the full path into destination buffer and returns
+ * pointer to path (skipping the preceding base path)
+ *
+ * @param dest  Destination buffer pointer
+ * @param base_path web server base path (i.e. /sd/www)
+ * @param uri   requested uri
+ * @param destsize  size of destination buffer
+ * @return
+ *      NULL when fail
+ *      Pointer to path, skipping the base
+ */
+const char* get_path_from_uri(char *dest, const char *base_path, const char *uri, size_t destsize);
+
+
+
+//#ifdef __cplusplus
+//}
+//#endif
+
+#endif /* COMPONENTS_KK_HTTP_APP_ */
